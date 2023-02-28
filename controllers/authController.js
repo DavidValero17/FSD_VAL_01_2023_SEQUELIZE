d044e89505094b4c2d314b07b8182ee4cf9461f7
David Valero <davidvalero17@hotmail.com>
2023-02-28 13:02:08 +0100

    we add a token wich can expire

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const { User } = require('../models');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const authController = {};
 
@@ -27,5 +28,30 @@ authController.register = async (req, res) => {
     }
     
 } 
+
+authController.login = async(req, res) => {
+    try {
+
+        const {email, password} = req.body;
+
+        const user = await User.findOne({where: {email: email}});
+
+        if(!user) {
+            return res.send('Wrong Credentials')
+        }
+        console.log(user.password)
+        const isMatch = bcrypt.compareSync(password, user.password);
+
+        if(!isMatch) {
+            return res.send('Wrong Credentials')
+        }
+
+        const token = jwt.sign({userId: user.id, email: user.email, roleId: user.role_id}, 'secreto', {expiresIn: '2h'});
+
+        return res.json(token)
+    } catch(error) {
+        return res.status(500).send(error.message);
+    }
+}
 
 module.exports = authController
